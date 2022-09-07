defb7315537bb3fe616101c3d046bcd9ff1e89b2
Abrie Bester <besterabrie6@gmail.com>
2022-09-07 10:48:25 +0200

    New pro 2 repo

---
+++ TodoAPI/TodoAPI/_34962913_DB_v1Context.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+
+namespace TodoAPI
+{
+    public partial class _34962913_DB_v1Context : DbContext
+    {
+        public _34962913_DB_v1Context()
+        {
+        }
+
+        public _34962913_DB_v1Context(DbContextOptions<_34962913_DB_v1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Device> Devices { get; set; } = null!;
+        public virtual DbSet<Zone> Zones { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("@Microsoft.Keyvault(SecretUri=https://keyvault027.vault.azure.net/secrets/key/ecec722b03364e20b0af6e1cdddb57b6)");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.ToTable("Category");
+
+                entity.Property(e => e.CategoryId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("CategoryID");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.ToTable("Device");
+
+                entity.Property(e => e.DeviceId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("DeviceID");
+
+                entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.ZoneId).HasColumnName("ZoneID");
+            });
+
+            modelBuilder.Entity<Zone>(entity =>
+            {
+                entity.ToTable("Zone");
+
+                entity.Property(e => e.ZoneId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("ZoneID");
+
+                entity.Property(e => e.DateCreated)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
